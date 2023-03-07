ee181828afc5e73a1ec55df78328019834f75751
RJ Sheperd <rjsheperd@gmail.com>
2023-03-07 13:56:03 -0800

    Figure out how compute pipeline is set up for copying arrays

--- src/app/renderer/core.cljs
+++ src/app/renderer/core.cljs
@@ -15,23 +15,83 @@
 (defn device [adapter] ^js/Promise
   (.requestDevice adapter))
 
-(defn ->pipeline [^js/GPUDevice device swap-chain-format]
-  (.createRenderPipeline device
-                         (clj->js
-                          {:vertex
-                           {:module
-                            (.createShaderModule device #js{:code vertex-shader-src})
-                            :entryPoint "main"}
-                           :fragment
-                           {:module
-                            (.createShaderModule device #js{:code fragment-shader-src})
-                            :entryPoint "main"
-                            :targets [{:format swap-chain-format}]}
-                           :primitive
-                           {:topology "triangle-list"}})))
-
 (defn command-encoder [device] ^js/CommandEncoder
   (.createCommandEncoder device))
+
+(def gpu-adapter (atom nil))
+(def gpu-device  (atom nil))
+
+(def gpu-buffer-usage-opts (js->clj js/GPUBufferUsage :keywordize-keys true))
+
+(defn ->gpu-buffer [device mapped? size usage-opts] ^js/GPUBuffer
+  (.createBuffer device #js {:mappedAtCreation mapped?
+                             :size             size
+                             :usage           (apply bit-or (map (partial get gpu-buffer-usage-opts) usage-opts))}))
+
+(defn gpu-buffer->array-buffer [^js/GPUBuffer gpu-buffer] ^js/ArrayBuffer
+  (.getMappedRange gpu-buffer))
+
+(defn write-to-gpu-buffer [gpu-buffer array-to-write]
+  (let [array-buffer (gpu-buffer->array-buffer gpu-buffer)]
+    ;; Write bytes to buffer.
+    (doto (js/Uint8Array. array-buffer)
+      (.set array-to-write))))
+
+(defn ->command-encoder [^js/GPUDevice device] ^js/GPUCommandEncoder
+  (.createCommandEncoder device))
+
+(defn ->copy-encoder [^js/GPUDevice device ^js/GPUBuffer gpu-write-buffer ^js/GPUBuffer gpu-read-buffer size]
+  (let [encoder (->command-encoder device)]
+    (.copyBufferToBuffer encoder
+                         gpu-write-buffer 
+                         0                
+                         gpu-read-buffer  
+                         0                
+                         size)
+    encoder))
+
+(defn ->pipeline [device ^js/GPUBuffer gpu-write-buffer ^js/GPUBuffer gpu-read-buffer] ^js/GPUCommandEncoder
+  ;; Write bytes to buffer.
+  (write-to-gpu-buffer gpu-write-buffer #js [1 2 3 4])
+
+  ;; Unmap buffer so it can be used later for copy.
+  (.unmap gpu-write-buffer)
+
+  (let [encoder  (->copy-encoder device gpu-write-buffer gpu-read-buffer 4)
+        commands (.finish encoder)]
+    ;; Create copy encoder b/w write and read buffers
+    (.submit (.-queue device) #js [commands])
+    encoder))
+
+(comment
+
+  (def device @gpu-device)
+
+  (def gpu-write-buffer (->gpu-buffer device true 4 [:MAP_WRITE :COPY_SRC]))
+
+  ;; Write bytes to buffer.
+  (write-to-gpu-buffer gpu-write-buffer #js [1 2 3 4])
+
+  ;; Unmap buffer so it can be used later for copy.
+  (.unmap gpu-write-buffer)
+
+  (def gpu-read-buffer (->gpu-buffer device false 4 [:COPY_DST :MAP_READ]))
+
+  (def copy-encoder (->copy-encoder device gpu-write-buffer gpu-read-buffer 4))
+  (def copy-commands (.finish copy-encoder))
+  (.submit (.-queue device) #js [copy-commands])
+
+  (def has-completed? (atom nil))
+  (go 
+    (let [completed? (<p! (.mapAsync gpu-read-buffer js/GPUMapMode.READ))]
+      (reset! has-completed? completed?)))
+
+  @has-completed?
+
+  (def copy-array-buffer (gpu-buffer->array-buffer gpu-read-buffer))
+  (println (js/Uint8Array. copy-array-buffer))
+
+  )
 
 (defn start-webgpu
   [^js/HTMLCanvasElement canvas]
@@ -39,25 +99,22 @@
   (go
     (if (not (. js/navigator -gpu))
       (js/alert "Your browser does not support WebGPU or it is not enabled. More info: https://webgpu.io")
-      (let [adapter ^js/GPUAdapter (<p! (.requestAdapter (. js/navigator -gpu)))
-            device ^js/GPUDevice (<p! (device adapter))
-            context (doto (.getContext canvas "webgpu")
-                      (.configure
-                       (clj->js
-                        {:device device
-                         :format swap-chain-format})))
-            pipeline (->pipeline device swap-chain-format)
-            command-enc (command-encoder device)
-            texture-view (-> context .getCurrentTexture .createView)
-            render-pass-desc {:colorAttachments
-                              [{:view texture-view
-                                :loadValue {:r 1.0 :g 1.0 :b 1.0 :a 1.0}}]}
-            pass-enc (.beginRenderPass command-enc (clj->js render-pass-desc))]
-        (doto pass-enc
-          (.setPipeline pipeline)
-          (.draw 3 1 0 0)
-          (.endPass))
-        (.submit (. device -queue) (clj->js [(.finish command-enc)]))))))
+      (let [adapter          ^js/GPUAdapter (<p! (.requestAdapter (. js/navigator -gpu)))
+            device           ^js/GPUDevice (<p! (device adapter))
+            gpu-write-buffer ^js/GPUBuffer (->gpu-buffer device true 4 [:MAP_WRITE :COPY_SRC])
+            gpu-read-buffer  ^js/GPUBuffer (->gpu-buffer device false 4 [:COPY_DST :MAP_READ])
+            pipeline         ^js/GPUCommandEncoder (->pipeline device gpu-write-buffer gpu-read-buffer)]
+
+        (println "Found GPU Adapter:" adapter)
+        (reset! gpu-adapter adapter)
+
+        (println "Found device:" device)
+        (reset! gpu-device device)
+
+        (println "Executing pipeline:" pipeline)
+        (<p! (.mapAsync gpu-read-buffer js/GPUMapMode.READ))
+
+        (println "Reading from Completion Buffer:" (js/Uint8Array. (gpu-buffer->array-buffer gpu-read-buffer)))))))
 
 (defn output []
   (r/create-class
